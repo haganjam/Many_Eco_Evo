bc9d991c0a02d8b07e5764b5997e668f75fde15b
James Hagan <james_hagan@outlook.com>
2021-03-09 09:20:12 +0100

    export the glm outputs

--- many_eco_evo_James_Hagan_out_of_sample_predictions.R
+++ many_eco_evo_James_Hagan_out_of_sample_predictions.R
@@ -57,21 +57,56 @@ euc_os.m <-
                      ~ sd(., na.rm = TRUE) ) )
 
 
+# summary of the models
+summary(lm_out_glm[[3]])
+lm_out_glm[[3]]$df.residual
+summary(lm_out_glm[[4]])
+lm_out_glm[[4]]$df.re
+summary(lm_out_glm[[5]])
+lm_out_glm[[5]]$df.re
+
+
 # five models from Table 2
+ci.out <- 
+  lapply(lm_out_glm[2:5], function(model) {
+  
+  df.m <- predict(object = model, newdata = euc_os.m, se.fit = TRUE)
+  
+  data.frame(SurveyID = c("Q1", "Q2", "Q3"),
+             fit = df.m$fit,
+             se.fit = df.m$se.fit,
+             ci.low = df.m$fit - (1.96*df.m$se.fit),
+             ci.high = df.m$fit - (1.96*df.m$se.fit))
+  
+})
 
+# write these to .csv files
+for (i in 1:length(ci.out)) {
+  
+  write_csv(x = ci.out[[i]], 
+            file = paste(here("figures_tables"), "/JamesHagan-predictions.", i, ".csv", sep = "") )
+  
+}
 
-y <- lm_out_glm[[2]]
-y
+# get predictions from the two quantile regression models and output to .csv
+predict(object = qm_1, newdata = euc_os.m, interval = "confidence", level = 0.95) %>%
+  as.data.frame(.) %>%
+  mutate(SurveyID = c("Q1", "Q2", "Q3")) %>%
+  select(SurveyID, fit, ci.low = lower, ci.high = higher) %>%
+  write_csv(x = ., file = here("figures_tables/quantile_regression_prediction_1.csv"))
 
-df.m <- predict(object = lm_out_glm[[2]], newdata = euc_os.m, se.fit = TRUE)
+# get t-value from coefficient that best answers the question
+summary.rq(qm_1, se = "boot", bsmethod= "wild")
+qm_1
 
-data.frame(SurveyID = c("Q1", "Q2", "Q3"),
-           fit = df.m$fit,
-           se.fit = df.m$se.fit,
-           ci.low = df.m$fit - (1.96*df.m$se.fit),
-           ci.high = df.m$fit - (1.96*df.m$se.fit))
+predict(object = qm_2, newdata = euc_os.m, interval = "confidence", level = 0.95) %>%
+  as.data.frame(.) %>%
+  mutate(SurveyID = c("Q1", "Q2", "Q3")) %>%
+  select(SurveyID, fit, ci.low = lower, ci.high = higher) %>%
+  write_csv(x = ., file = here("figures_tables/quantile_regression_prediction_2.csv"))
 
-
+# get t-value from coefficient that best answers the question
+summary.rq(qm_2, se = "boot", bsmethod= "wild")
 
 
 
